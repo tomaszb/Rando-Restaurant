dbb1158fee3cac36a12e7254d283de578b1a8a1e
tomaszb <tbacewicz@gmail.com>
2021-02-01 22:33:01 -0500

    initial commit

---
+++ api/googleAPI.py
@@ -0,0 +1,55 @@
+import googlemaps
+from datetime import datetime
+
+class findRestaurants:
+    def __init__(client, location, min_price=1, max_price=4, radius=3000, type="restaurant"):
+        self.location = location
+        self.min_price = min_price
+        self.max_price = max_price
+        self.radius = radius
+        self.type = type
+        self.client = client
+
+
+    def getPages(self, pages=3):
+        i = 0
+        results = []
+        results = results + 
+        next_page = ""
+
+        try:
+            full_result = self.client.places_nearby(location = self.location
+                                      min_price = self.min_price
+                                      max_price = self.max_price
+                                      open_now = True
+                                      radius = self.radius
+                                      type = "restaurant")
+
+        except ApiError as err:
+            return "Api Error: {0}".format(err)
+        except Exception as e:
+            return "Uncaught Error: {0}".format(e)
+            
+        if full_result['status'] == 'OK':
+            results = results + full_result['results']
+            next_page = full_result['next_page_token']
+
+        else:
+            return "Error"
+
+        while (i < 2) and next_page:
+            full_result = self.client.places_nearby(location = self.location
+                                      min_price = self.min_price
+                                      max_price = self.max_price
+                                      open_now = True
+                                      radius = self.radius
+                                      type = "restaurant")
+            
+            if full_result['status'] == 'OK':
+                results = results + full_result['results']
+                next_page = full_result['next_page_token']
+
+            i = i+1
+
+        return results
+ 
